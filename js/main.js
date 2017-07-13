02f03c090479e1cc1861dbea52bfb7a5a9e8da74
Barry White <barry505@gmail.com>
2017-07-13 14:52:35 -0500

    Daily file updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-	
+var numberOfSquares = 256;
+var square = '<div class="gridSquare"></div>';
+
+function makeGrid() {
+  for(i = 0; i < numberOfSquares; i++) {
+    $('#grid').append(square);
+  };
+}
+
+function clearGrid() {
+	$('.gridSquare').removeClass('gridChange');
+}
+
+$(document).ready(function() {
+
+  makeGrid();
+
+  $('.gridSquare').mouseover(function() {
+    $(this).addClass('gridChange');
+  });
 }); // end ready
